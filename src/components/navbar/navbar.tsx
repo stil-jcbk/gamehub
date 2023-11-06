45bf7b42280ee1b4d07052291e9ac41c50f3654d
stil-jcbk <kubkon134@gmail.com>
2023-11-06 13:22:17 +0100

    added navbar

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,13 @@
+import "./style.scss"
+import {Link} from "react-router-dom"
+
+export default function Navbar(){
+    return(
+        <div className="navbar">
+            <img className="logo" src="/logo.svg" alt=""/>
+            <div className="routes">
+                <Link to="/">HOME</Link>
+            </div>
+        </div>
+    )
+}
