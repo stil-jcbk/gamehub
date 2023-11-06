10354cc5a0e802738cbfdfc65c3dd8c3940224cb
stil-jcbk <kubkon134@gmail.com>
2023-11-06 13:24:10 +0100

    added hash router and removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import {createHashRouter, RouterProvider} from "react-router-dom"
+import Layout from "./pages/layout/layout";
+import HomePage from "./pages/home/home";
+
+const router = createHashRouter([{
+  element: <Layout/>,
+  children: [
+    {
+      path: "/",
+      index: true,
+      element: <HomePage/>
+    }
+  ]
+}])
 
 function App() {
   return (
     <div className="App">
-
+      <RouterProvider router={router}/>
     </div>
   );
 }
