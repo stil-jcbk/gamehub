389bddeee0aa7cb518a92e93459a6a5c4893646f
stil-jcbk <kubkon134@gmail.com>
2024-01-08 14:12:49 +0100

    changed the way button takes innerText prop

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,13 +2,13 @@ import "./style.scss"
 import {MouseEventHandler} from "react";
 
 type ButtonProps = {
-    text: string;
+    children: string;
     onClick: MouseEventHandler;
 }
 export default function Button(props: ButtonProps){
     return(
         <div onClick={props.onClick} className="btn">
-            <span className="btn-text"></span>
+            <span className="btn-text">{props.children}</span>
         </div>
     )
 }
